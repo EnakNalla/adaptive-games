822d38b5430b0b8fe55297851241c68870b9aa21
EnakNalla <121622489+EnakNalla@users.noreply.github.com>
2023-01-30 21:04:42 +0000

    build(repo): initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  extends: ["prettier", "eslint:recommended"],
+  overrides: [
+    {
+      extends: [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking"
+      ],
+      files: ["**/*.ts", "**/*.tsx"],
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ["./tsconfig.json", "./apps/*/tsconfig.json", "./packages/*/tsconfig.json"]
+      }
+    }
+  ],
+  root: true,
+  reportUnusedDisableDirectives: true,
+  ignorePatterns: [".eslintrc.js", "**/*.config.js", "**/*.config.cjs", "packages/config/**"]
+};
