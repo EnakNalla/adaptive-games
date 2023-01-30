38afc860b3c5f1361eb959bb81cb39997671d044
EnakNalla <121622489+EnakNalla@users.noreply.github.com>
2023-01-30 22:08:30 +0000

    build(ui): add ui library w/ bootstrap

---
+++ packages/ui/.storybook/preview.js
@@ -0,0 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export const parameters = {
+  actions: {argTypesRegex: "^on[A-Z].*"},
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/
+    }
+  },
+  theme: {
+    selector: "html",
+    dataAttr: "data-bs-theme"
+  }
+};
